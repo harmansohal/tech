c7ca2f6c7a890c500cd04747db18c5d1b2758e78
harmansohal <harmanjitkaur1@my.jcu.edu.au>
2016-10-15 13:08:26 +1000

    home page changes
    i have made home page with some changes

--- wp-content/themes/sydney-child/functions.php
+++ wp-content/themes/sydney-child/functions.php
@@ -11,4 +11,13 @@ function my_theme_enqueue_styles() {
     );
 }
 add_action( 'wp_enqueue_scripts', 'my_theme_enqueue_styles' );
+
+
+add_filter( 'wp_nav_menu_items', 'sydney_child_add_top_search_menu', 10, 2 );
+function sydney_child_add_top_search_menu( $items, $args ) {
+    if ($args->theme_location == 'primary') {
+        $items .= '<li class="top-search-menu">'.get_search_form(false).'</li>';
+    }
+    return $items;
+}
 ?>
