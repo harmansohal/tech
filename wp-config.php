337670adb7409b61a28370668dfdb530792dea1e
harmansohal <harmanjitkaur1@my.jcu.edu.au>
2016-11-08 11:24:49 +1000

    some more changes

--- wp-config.php
+++ wp-config.php
@@ -23,10 +23,10 @@
 define('DB_NAME', 'tech');
 
 /** MySQL database username */
-define('DB_USER', 'tech');
+define('DB_USER', 'manib487_tech');
 
 /** MySQL database password */
-define('DB_PASSWORD', 'admin123');
+define('DB_PASSWORD', 'sohal');
 
 /** MySQL hostname */
 define('DB_HOST', 'localhost');
